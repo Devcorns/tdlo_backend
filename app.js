db1df25225ed526a6d95085e3df8a8abe029f0a3
Prakhar <prakhar@isglobalweb.com>
2019-04-15 17:18:33 +0530

    backend

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('*', cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/user', usersRouter);
 app.use('/api/auth', authRouter);
 
 
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log(req,res,next)
+router.get('/baseinfo', function(req, res, next) {
+  console.log(req,res,next);
   res.send('respond with a resource');
 });
 
