8cc96d5f25dbf666ef8c44110c6ab60f29b48c84
Prakhar <prakhar@isglobalweb.com>
2019-05-10 17:33:59 +0530

    get company

--- model/company-details/cmp-schema.js
+++ model/company-details/cmp-schema.js
@@ -6,3 +6,19 @@ var company_profile = new Schema({
 });
 
 module.exports = mongoose.model('company_profile');
+
+// db.company_profile.insertOne({
+	
+// 	"name":"DC",
+// 	"strength":17,
+// 	"origin":2017,
+//    "dept":{"it":6,"acct":5,"hr":2,"mngr":3,"ownr":1},
+//    "tech":['Node','PhP','MongoDB','Javascript','Android','Angular'],
+// 	"empList":['Empid1','Empid2'],
+// 	"size":'Small',
+// 	"location":'noida',
+// 	"Nation":'india',
+// 	"type":'local'
+	
+
+// })
