8af22dc427f802b3e1a06d7207aa4d249d6e1872
Prakhar <prakhar@isglobalweb.com>
2019-05-17 11:59:00 +0530

    rating profile dir or employee detail dir

--- model/employee-details/add-employee.js
+++ model/employee-details/add-employee.js
@@ -1,30 +1,34 @@
 var mongoose = require("mongoose");
 var dbConfig = require("./../dbconfig");
-var companyData;
+var employeeData;
 var viewProfile = {
     
-    get:function(callback,params) {
+    add:function(callback,params) {
         
         dbConfig.connect();
         var db = dbConfig.getMongooseConnection();
         
         // Mongoose model compiled once issue generated so we use try here so we use try to handle this issue    
         try {
-            console.log("INslide view profile .js ",params.query.search_term);
-            companyData = mongoose.model('company_profile', {name:String},'company_profile');
+
+            console.log("INslide Add Employee .js ", params.query.search_term);
+            employeeData = mongoose.model('company_profile', {name : String}, 'company_profile');
             viewData();
-            
-            
+
         } catch (error) {
+
             console.log(error)
-            companyData = new mongoose.model('company_profile');
-             viewData();
+            employeeData = new mongoose.model('company_profile');
+            viewData();
+
         }
+
         function viewData() {
-            var k = params.query.search_term
-            console.log(" Get ISG in viewprofile.js ","params.query.search_term => ",typeof params.query.search_term,"companyData =>",companyData);
+
+            var k = params.query.search_term;
+            console.log(" Get ISG in viewprofile.js ", "params.query.search_term => ", typeof params.query.search_term, "companyData =>", companyData);
             
-            companyData.findOne({"_id":params.query.search_term},function(err, data) {
+            employeeData.findOne({"_id":params.query.search_term},function(err, data) {
                // if (err) return handleError(err);
                 dbConfig.close();
                 callback(data);
--- model/employee-details/view-employee.js
+++ model/employee-details/view-employee.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var dbConfig = require("./../dbconfig");
-var companyData;
+var employeeData;
 var viewProfile = {
     
     get:function(callback,params) {
@@ -10,21 +10,21 @@ var viewProfile = {
         
         // Mongoose model compiled once issue generated so we use try here so we use try to handle this issue    
         try {
-            console.log("INslide view profile .js ",params.query.search_term);
-            companyData = mongoose.model('company_profile', {name:String},'company_profile');
+            console.log("INslide Employee view profile .js ",params.query.search_term);
+            employeeData = mongoose.model('company_profile', {name:String},'company_profile');
             viewData();
             
             
         } catch (error) {
             console.log(error)
-            companyData = new mongoose.model('company_profile');
+            employeeData = new mongoose.model('company_profile');
              viewData();
         }
         function viewData() {
             var k = params.query.search_term
             console.log(" Get ISG in viewprofile.js ","params.query.search_term => ",typeof params.query.search_term,"companyData =>",companyData);
             
-            companyData.findOne({"_id":params.query.search_term},function(err, data) {
+            employeeData.findOne({"_id":params.query.search_term},function(err, data) {
                // if (err) return handleError(err);
                 dbConfig.close();
                 callback(data);
